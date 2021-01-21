5842edb81b92675568c20426c950c41fbf68d43d
omarelbrolosy <omarelbrolosy@gmail.com>
2021-01-21 22:00:35 +0200

    adding robot and sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,11 +77,12 @@ module.exports = {
       },
     },
     `gatsby-plugin-advanced-sitemap`,
+    `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
+        name: `Omar Elbrolosy`,
+        short_name: `elbrolosy`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
